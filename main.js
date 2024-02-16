669957e767183a912d1e6a3d18f65f8daf59cdd1
Geane Farias <fariassantos.geane@gmail.com>
2024-02-16 17:09:38 -0300

    Integração do sistema de adicionar imagens

--- main.js
+++ main.js
@@ -8,8 +8,19 @@ $(document).ready(function(){
     })
 
     $('form').on('submit',function(e) {
-        console.log('submit')
         e.preventDefault();   
+        const enderecoImagemNova = $('#Endereco-Imagem-nova').val();
+        const novoItem = $('<li style="display: none"></li>');
+        $(`<img src="${enderecoImagemNova}"/>`).appendTo(novoItem)
+        $(`
+            <div class="overlay-imagem-link">
+            <a href="${enderecoImagemNova}" target="_blank" title="Ver imagem></a>
+            Ver imagem
+            </div>
+        `).appendTo(novoItem)
+        $(novoItem).appendTo('ul')
+        $(novoItem).fadeIn(2000);
+        $('#Endereco-Imagem-nova').val(" ")
     })
     
 })
