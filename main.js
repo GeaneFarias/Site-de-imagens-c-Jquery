5f8a3e4417b04cedd849d007879f3978fd603cd3
Geane Farias <fariassantos.geane@gmail.com>
2024-02-16 16:27:19 -0300

    Efeitos incluidos e imagens ajustadas

---
+++ main.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $('header button').click(function(){
+        $('form').slideDown();
+    })
+
+    $('#cancel').click(function(){
+        $('form').slideUp();
+    })
+
+    $('form').on('submit',function(e) {
+        console.log('submit')
+        e.preventDefault();   
+    })
+    
+})
+
